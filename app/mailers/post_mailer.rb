8e4b7a5d2fde8669510a63cacfaf02bed8401dcd
Quy <quy.nguyenngoctp@gmail.com>
2017-08-18 14:35:06 +0700

    commit for heroku

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -6,7 +6,8 @@ class PostMailer < ApplicationMailer
   #   en.post_mailer.notify_new_post.subject
   #
   def notify_new_post(post)
-    @post = post
-    mail to: "#{@post.poster.name}", subject: "#{@post.poster.name} wrote on your wall"
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
   end
 end
